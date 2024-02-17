2ef53efd2cfd8f205d31333769cade64bd180ee2
Leonardo Jacomussi <leonardojacomussi@gmail.com>
2024-02-17 19:34:58 -0300

    BUILD: adjust setting for github pages

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: process.env.NODE_ENV !== "development" ? "https://leonardojacomussi.github.io/rocketnotes-ui" : "/",
+  base: process.env.NODE_ENV !== "development" ? "/rocketnotes-ui" : "/",
 });
